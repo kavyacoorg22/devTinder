1f8a5e8cf84361f36e059e28f48bffdca3e92664
Kavya <kavyakopatti99@gmail.com>
2024-10-23 19:14:01 +0530

    basic router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express=require("express");
 const app=express();
 
-app.use((req,res)=>
+app.use("/hello/2",(req,res)=>
+  {
+    res.send("abraka dabra");
+  })
+app.use("/",(req,res)=>
 {
   res.send("hiii helloo sangeetha")
 })
 
+app.use("/user",(req,res)=>
+{
+  res.send("HAHAHAHAHAHS");
+})
+app.get("/user",(req,res)=>
+{
+  res.send({firstname:"kavya", secondname:"ms"});
+})
+
+app.post("/user",(req,res)=>
+{
+  res.send("data saved successfully");
+})
+
+app.delete("/user",(req,res)=>
+{
+  res.send("data deleted");
+})
 app.listen(7777,()=>console.log("server is running on 7777 port"))
