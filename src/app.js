1ef8b209b57236a140059877571164d5e4dfdf96
Kavya <kavyakopatti99@gmail.com>
2024-10-24 19:50:15 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 const express=require("express");
 const app=express();
 
-app.use("/hello/2",(req,res)=>
+
+
+app.use("/admin",(req,res,next)=>
+{
+  console.log("middleware is checked");
+  const token="xyz";
+  const isAuthorized=token==="xyz";
+  if(!isAuthorized)
   {
-    res.send("abraka dabra");
+    res.send("UnAuthorized");
+  }
+  else{
+    next();
+  }
+});
+  app.use("/admin/userdata",(req,res,next)=>
+  {
+    res.send("user data");
   })
-app.use("/",(req,res)=>
-{
-  res.send("hiii helloo sangeetha")
-})
-
-app.use("/user",(req,res)=>
-{
-  res.send("HAHAHAHAHAHS");
-})
-app.get("/user",(req,res)=>
-{
-  res.send({firstname:"kavya", secondname:"ms"});
-})
-
-app.post("/user",(req,res)=>
-{
-  res.send("data saved successfully");
-})
-
-app.delete("/user",(req,res)=>
-{
-  res.send("data deleted");
+    
+app.use("/admin/deleteuser",(req,res,next)=>{
+  res.send("user data deleted");
+ 
 })
 app.listen(7777,()=>console.log("server is running on 7777 port"))
