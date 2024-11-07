64b179b924ae55cd653f3bafc7bd89330547b8c1
Kavya <kavyakopatti99@gmail.com>
2024-11-07 19:18:16 +0530

    validations

--- src/app.js
+++ src/app.js
@@ -8,15 +8,74 @@ app.use(express.json());
 app.post('/signup',async(req,res)=>{
   const user=new User(req.body);
     try{
-    user.save();
-    res.send("data saved successfully")
+   await  user.save();
+    res.send("data saved successfully");
     }catch(err)
     {
-      res.status(400).send("Something went wrong");
+      res.status(400).send("Something went wrong"+err.message);
     }
 })
 
+app.get('/user',async(req,res)=>{
+   try
+   {
+     const user=await User.findOne({firstName:req.body.firstname});
+     if(!user)
+     {
+      res.status(404).send('user is not fund')
+     }else
+     {
+      res.send(user);
+     }
+   }
+   catch(err)
+   {
+   console.log("Something went wrong");
+   }
+})
 
+app.delete('/user',async(req,res)=>
+{
+  const userId=req.body.userId;
+  
+  try{
+   const user= await User.findByIdAndDelete({userId});
+    
+      res.send("User deleted succesfully");
+    
+  }
+  catch(err)
+  {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+app.patch('/user/:userId',async(req,res)=>
+{
+
+  const userId=req.params?.userId;
+  const data=req.body;
+  try{
+    const ALLOWED_UPDATES=["about",'gender','age',"skill","photourl"];
+    const isUpdateAllowed=Object.keys(data).every((k)=>
+    ALLOWED_UPDATES.includes(k));
+    if(!isUpdateAllowed)
+    {
+      throw new Error('update not possible');
+    }
+    if(data?.skill.length>10)
+    {
+      throw new Error("Skill can't be more than 10");
+    }
+    const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:'after',runValidators:true});
+    console.log(user);
+res.send("User updated successfully");
+
+  }catch(err)
+  {
+    res.status(400).send('Something went wrong'+err.message);
+  }
+});
 
 
 connectDB()
--- src/modules/user.js
+++ src/modules/user.js
@@ -1,27 +1,63 @@
 const mongoose=require("mongoose");
+const validator=require("validator");
 
 const userSchema=new mongoose.Schema({
     firstName:{
-      type:String
+      type:String,
+      minlength:4,
+      required:true,
     },
     lastName:{
       type:String
     },
     emailId:{
-      type:String
+      type:String,
+      unique:true,
+      required:true,
+      lowercase:true,
+      trim:true,
+      validate(value){
+        if(!validator.isEmail(value))
+          throw new Error('invalid email address'+value);
+      }
     },
     password:{
-      type:String
+      type:String,
+      required:true
     },
     age:{
-      type:Number
+      type:Number,
+      min:18
     },
     gender:
     {
-      type:String
+      type:String,
+      validate(value)
+
+      {
+        if(!["male","female","other"].includes(value))
+        {
+          throw new Error("Gender is not valid")
+        }
+      }
+    },
+    about:{
+      type:String,
+      default:"This is the default value to the about"
+    },
+    skill:{
+      type:[String]
+    },
+    photourl:{
+      type:String,
+      default:"https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small_2x/user-profile-icon-free-vector.jpg",
+      validate(value){
+        if(!validator.isURL(value))
+          throw new Error('invalid photo URL');
+      }
     }
 
-  }
+  },{timestamps:true}
 )
 
 
